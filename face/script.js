a9ce8f54053cd4e7bffdf26da56815b9fa98b786
GeekCoder403 <124810233+Mpouel@users.noreply.github.com>
2025-05-02 07:53:55 -0400

    ds

--- face/script.js
+++ face/script.js
@@ -6,23 +6,35 @@ let lastTriggerTime = 0;
 const triggerCooldown = 5000;
 let startTime = new Date().getTime();
 
-function onOpenCvReady() {
+function waitForOpencv(callbackFn) {
+  const check = () => {
+    if (cv && cv.Mat) callbackFn();
+    else setTimeout(check, 100);
+  };
+  check();
+}
+
+waitForOpencv(() => {
   console.log('OpenCV.js is ready');
   loadFaceCascade();
   listCameras();
-}
+});
 
 function loadFaceCascade() {
-  let cascadeFile = 'haarcascade_frontalface_default.xml';
-  let url = 'https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/' + cascadeFile;
+  const cascadeFile = 'haarcascade_frontalface_default.xml';
+  const url = 'https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/' + cascadeFile;
   fetch(url)
     .then(res => res.arrayBuffer())
     .then(buffer => {
       cv.FS_createDataFile('/', cascadeFile, new Uint8Array(buffer), true, false, false);
       faceCascade = new cv.CascadeClassifier();
-      faceCascade.load(cascadeFile);
-      console.log('Face cascade loaded');
-    });
+      if (!faceCascade.load(cascadeFile)) {
+        console.error('Failed to load cascade classifier');
+      } else {
+        console.log('Face cascade loaded');
+      }
+    })
+    .catch(err => console.error('Cascade fetch error:', err));
 }
 
 function listCameras() {
@@ -44,53 +56,57 @@ function listCameras() {
 }
 
 function startWebcam(deviceId) {
-  if (video.srcObject) video.srcObject.getTracks().forEach(track => track.stop());
+  if (video.srcObject) {
+    video.srcObject.getTracks().forEach(track => track.stop());
+  }
   navigator.mediaDevices.getUserMedia({ video: { deviceId } })
     .then(stream => {
       video.srcObject = stream;
-      video.onloadeddata = processVideo;
+      video.onloadeddata = () => {
+        video.play();
+        processVideo();
+      };
     })
     .catch(err => console.error('Webcam error:', err));
 }
 
 function processVideo() {
-  let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
-  let gray = new cv.Mat();
-  let faces = new cv.RectVector();
+  const src = new cv.Mat(canvas.height, canvas.width, cv.CV_8UC4);
+  const gray = new cv.Mat();
+  const faces = new cv.RectVector();
 
   function processFrame() {
     if (!video.srcObject) return;
 
-    let begin = Date.now();
     context.drawImage(video, 0, 0, canvas.width, canvas.height);
-    let imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
     src.data.set(imageData.data);
 
     cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
     cv.equalizeHist(gray, gray);
     faceCascade.detectMultiScale(gray, faces, 1.1, 3, 0);
 
-    let now = new Date().getTime();
+    const now = new Date().getTime();
 
     if (faces.size() === 2 && now - lastTriggerTime > triggerCooldown) {
       lastTriggerTime = now;
-      let t = (now - startTime) / 1000;
+      const t = (now - startTime) / 1000;
       console.log(`2 faces detected at ${t.toFixed(2)}s`);
 
-      let rect = faces.get(1);
-      let faceImageData = context.getImageData(rect.x, rect.y, rect.width, rect.height);
+      const rect = faces.get(1);
+      const faceImageData = context.getImageData(rect.x, rect.y, rect.width, rect.height);
 
-      let faceCanvas = document.createElement('canvas');
+      const faceCanvas = document.createElement('canvas');
       faceCanvas.width = rect.width;
       faceCanvas.height = rect.height;
       faceCanvas.getContext('2d').putImageData(faceImageData, 0, 0);
 
-      let link = document.createElement('a');
+      const link = document.createElement('a');
       link.download = 'second_face_screenshot.png';
       link.href = faceCanvas.toDataURL();
       link.click();
 
-      let img = new Image();
+      const img = new Image();
       img.src = faceCanvas.toDataURL();
       img.classList.add('screenshot');
       document.getElementById('screenshotContainer').appendChild(img);

55927dc457b6bac987a119cb995eca7fc3bd7794
GeekCoder403 <124810233+Mpouel@users.noreply.github.com>
2025-05-03 14:05:41 -0400

    f

--- face/script.js
+++ face/script.js
@@ -1,32 +1,57 @@
 const video = document.getElementById('video');
-const box = document.getElementById('statusBox');
+const box = document.getElementById('box');
+const cameraSelect = document.createElement('select');
+document.body.insertBefore(cameraSelect, video);
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
   faceapi.nets.ageGenderNet.loadFromUri('/models')
-]).then(startVideo);
+]).then(getCameras);
 
-function startVideo() {
-  navigator.mediaDevices.getUserMedia({ video: {} })
-    .then(stream => {
-      video.srcObject = stream;
-    })
-    .catch(err => console.error('Error accessing camera:', err));
+let currentStream;
+
+async function getCameras() {
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  const videoDevices = devices.filter(device => device.kind === 'videoinput');
+
+  cameraSelect.innerHTML = '';
+  videoDevices.forEach((device, index) => {
+    const option = document.createElement('option');
+    option.value = device.deviceId;
+    option.text = device.label || `Camera ${index + 1}`;
+    cameraSelect.appendChild(option);
+  });
+
+  cameraSelect.addEventListener('change', () => startVideo(cameraSelect.value));
+
+  if (videoDevices.length > 0) {
+    startVideo(videoDevices[0].deviceId);
+  }
+}
+
+async function startVideo(deviceId) {
+  if (currentStream) {
+    currentStream.getTracks().forEach(track => track.stop());
+  }
+
+  const constraints = {
+    video: { deviceId: { exact: deviceId } }
+  };
+
+  currentStream = await navigator.mediaDevices.getUserMedia(constraints);
+  video.srcObject = currentStream;
 }
 
 video.addEventListener('play', () => {
-  const interval = setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withAgeAndGender();
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withAgeAndGender();
 
-    if (detections.length >= 2) {
-      const secondFace = detections[1];
-      if (secondFace.age > 25) {
-        box.style.backgroundColor = 'red';
-      } else {
-        box.style.backgroundColor = 'green';
-      }
+    if (detections.length >= 2 && detections[1].age > 25) {
+      box.style.background = 'red';
     } else {
-      box.style.backgroundColor = 'green';
+      box.style.background = 'green';
     }
   }, 1000);
 });
